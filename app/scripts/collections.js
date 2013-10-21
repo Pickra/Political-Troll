52637fd3dacbab94aeb861200c08599504e4822b
Pickra <picker10024@yahoo.com>
2013-10-21 14:13:30 -0400

    made a CollectionOfPolliticians. added collections script to the index.

--- app/scripts/collections.js
+++ app/scripts/collections.js
@@ -1,8 +1,8 @@
-// var CollectionA = Backbone.Collection.extend({
-// 	initialize: function(){
-// 		this.on('add', function(A) {
-// 			new ModelA({model:A})
-// 		})
-// 	},
-// 	model: ModelA
-// }) ---------- i don't need this cuz there's only going to be 1 modelA as opposed to alotta bills or politicians?
+var CollectionOfPoliticians = Backbone.Collection.extend({
+	initialize: function(){
+		this.on('add', function(A) {
+			new Politician({model:Politician})
+		})
+	},
+	model: Politician
+})
