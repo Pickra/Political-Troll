01cc495a1d70d7c5f4bce6980e09a7ef6af9e819
Pickra <picker10024@yahoo.com>
2013-10-30 16:12:44 -0400

    made a Bill model.

--- app/scripts/models.js
+++ app/scripts/models.js
@@ -6,8 +6,11 @@ State = Backbone.Model.extend({
 Member = Backbone.Model.extend({
 	initialize: function(){
 		console.log('this is ', this);
-
 	},
-	
 })
 
+Bill = Backbone.Model.extend({
+	initialize: function(){
+		console.log('this is, ', this);
+	}
+})
