47befc04f80457dda336565df4a28de69e98adc2
Pickra <picker10024@yahoo.com>
2013-11-18 10:50:46 -0500

    altered gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -192,6 +192,12 @@ module.exports = function (grunt) {
                     cwd: '<%= yeoman.app %>/images',
                     src: '{,*/}*.{png,jpg,jpeg}',
                     dest: '<%= yeoman.dist %>/images'
+                }, {
+                    expand : true,
+                    flatten : true,
+                    cwd : '<%= yeoman.app %>/styles',
+                    src : '**/*.{png,jpg,jpeg,gif}',
+                    dest : '<%= yeoman.dist %>/styles'
                 }]
             }
         },
